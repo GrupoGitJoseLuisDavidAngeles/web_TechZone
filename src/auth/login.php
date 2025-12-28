bd76c20f12f674359b6aa8bdbc60053e3031258a
DavidAvilaSanchez <2222832@alu.murciaeduca.es>
2025-12-28 12:32:57 +0100

    formulario login

--- src/auth/login.php
+++ src/auth/login.php
@@ -14,55 +14,19 @@
 </head>
 
 <body>
-    <header>
-        <div class="logo"></div>
-
-        <div>
-            <label for="">Categorías</label>
-            <select name="" id="" class="selectCategorias">
-                <option value="">Portátiles</option>
-                <option value="">Sobremesa</option>
-                <option value="">Componentes</option>
-                <option value="">Periféricos</option>
-            </select>
-        </div>
-
-        <div class="divBusqueda">
-            <input class="inputBusqueda" type="text" name="" id="" placeholder="Busque un producto">
-            <span class="imagenLupa"></span>
-        </div>
-
-        <span>Iniciar sesión</span>
-        <span>Carrito</span>
-    </header>
-
-    <aside class="asideIzquierdo"></aside>
-    <aside class="asideDerecho"></aside>
-
     <main>
-        <div class="container-form">
-
-            <h2> Inicia sesión </h2>
-            <form method="post" class="loginform">
-                <label for="tEmail">Cuenta de correo:</label>
+            <h2> Inicio de sesión </h2>
+            <form method="post" class="loginForm">
+                <label for="tEmail">Identificador de usuario:</label>
                 <input type="email" name="email" id="tEmail">
                 <label for="tPassword">Contraseña: </label>
                 <input type="password" name="password" id="tPassword">
                 <button type="submit">Iniciar sesión</button>
-                <button type="button">Registrase</button>
+                <label for="" class="register-link"><a href="register.php">O registrarse</a></label>
+                <!-- <button type="button"></button> -->
             </form>
-        </div>
-    </main>
-    <footer>
-        <nav>
-            <ul class="listaFooter">
-                <li>Contacto</li>
-                <li>Sobre nosotros</li>
-                <li>Comunidad</li>
-            </ul>
-        </nav>
-        <p class="pCopyright">Copyright</p>
 
+    </main>
 </body>
 
 </html>
