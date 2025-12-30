70c5b60bf5d0f0e1705721421e7a35aba890a663
DavidAvilaSanchez <2222832@alu.murciaeduca.es>
2025-12-30 11:38:12 +0100

    productos

--- src/app/controllers/AuthCotroller.php
+++
@@ -1,9 +0,0 @@
-<?php
-class AuthCotroller 
-{
-    public function login() {
-        
-    }
-}
-
-?>
--- src/public/productos.js
+++
@@ -1,9 +0,0 @@
-import productosService from "../services/productos.service.js";
-setup();
-
-async function setup() {
-    const service = new productosService();
-    const productos = service.getProducts();
-    // console.log(productos);
-
-}
