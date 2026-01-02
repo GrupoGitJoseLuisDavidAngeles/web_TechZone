92949313b6006e72a22cc6b8ca69a37af1df3f00
Ángeles <1743804@alu.murciaeduca.es>
2026-01-02 05:16:52 +0100

    Actualización de carrito

--- src/carrito/cart.php
+++ src/carrito/cart.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Carrito</title>
     <link rel="stylesheet" href="./cart.css">
+    <script type="module" src="./cart.js"></script>
 </head>
 
 <body>
@@ -17,7 +18,7 @@
             <span class="imagenLupa"></span>
         </div>
 
-        <span>Iniciar sesión</span>
+        <span class="login">Iniciar sesión</span>
     </header>
 
     <aside class="asideLeft"></aside>
@@ -29,78 +30,78 @@
 
             <div class="productsContainer">
 
-                <div class="product">
+                <!-- <div class="product">
                     <div class="productImage1"></div>
     
                     <div class="productInformation">
-                        <p>Teclado mecánico</p>
+                        <p class="productName">Teclado mecánico</p>
+                        <p class="category">Sección a la que pertenece</p>
                         <p class="deliveryDay">Recíbelo del 6 al 9 de enero</p>
-                        <button class="btnDelete">-</button>
-                        <span class="productQuantity" id="itemQuantity">5</span>
-                        <button class="btnAdd">+</button>
+
+                        <div class="quantityControls">
+                            <button class="btnDelete">-</button>
+                            <span class="productQuantity" id="itemQuantity">5</span>
+                            <button class="btnAdd">+</button>
+                        </div>
                         <a href="" class="deleteProduct">Eliminar producto</a>
                     </div>
-                    <p class="price">50 €</p>
+                    <p class="price"><span class="amount">30</span> €</p>
                 </div>
 
                 <div class="product">
                     <div class="productImage2"></div>
     
                     <div class="productInformation">
-                        <p>Ratón gaming</p>
-                        <p class="deliveryDay">Recibelo el 2 de enero</p>
-                        <button class="btnDelete">-</button>
-                        <span class="productQuantity" id="productQuantity">2</span>
-                        <button class="btnAdd">+</button>
+                        <p class="productName">Ratón gaming</p>
+                        <p class="category">Sección a la que pertenece</p>
+                        <p class="deliveryDay">Recíbelo el 2 de enero</p>
+
+                        <div class="quantityControls">
+                            <button class="btnDelete">-</button>
+                            <span class="productQuantity" id="productQuantity">2</span>
+                            <button class="btnAdd">+</button>
+                        </div>
                         <a href="" class="deleteProduct">Eliminar producto</a>
                     </div>
-                    <p class="price">25 €</p>
+                    <p class="price"><span class="amount">50</span> €</p>
                 </div>
                 
                 <div class="product">
                     <div class="productImage3"></div>
                     
                     <div class="productInformation">
-                        <p>Monitor FHD</p>
-                        <p class="deliveryDay">Recibelo mañana</p>
-                        <button class="btnDelete">-</button>
-                        <span class="productQuantity" id="productQuantity">1</span>
-                        <button class="btnAdd">+</button>
+                        <p class="productName">Monitor FHD</p>
+                        <p class="category">Sección a la que pertenece</p>
+                        <p class="deliveryDay">Recíbelo mañana</p>
+
+                        <div class="quantityControls">
+                            <button class="btnDelete">-</button>
+                            <span class="productQuantity" id="productQuantity">1</span>
+                            <button class="btnAdd">+</button>
+                        </div>
                         <a href="" class="deleteProduct">Eliminar producto</a>
                     </div>
-                    <p class="price">285 €</p>
-                </div>      
-
-
-                <div class="product">
-                    <div class="productImage3"></div>
-                    
-                    <div class="productInformation">
-                        <p>Monitor FHD</p>
-                        <p class="deliveryDay">Recibelo mañana</p>
-                        <button class="btnDelete">-</button>
-                        <span class="productQuantity" id="productQuantity">1</span>
-                        <button class="btnAdd">+</button>
-                        <a href="" class="deleteProduct">Eliminar producto</a>
-                    </div>
-                    <p class="price">285 €</p>
-                </div> 
-
-            </div>
+                    <p class="price"><span class="amount">280</span> €</p>
+                </div>
+            </div> -->
         </div>
 
         <div class="order">
             <h2>Resumen del pedido</h2>
             <div class="orderSummary">
-                <p>Precio de los productos <span class="subtotal">1000€</span></p>
-                <p>IVA <span class="iva">100€</span></p>
+                <p>Precio de los productos 
+                    <span class="subtotal"><span class="amount">1000</span> €</span>
+                </p>
+                <p>IVA 
+                    <span class="iva"><span class="amount">100</span> €</span>
+                </p>
                 <p>Gastos de envío</p>
             </div>
             <div class="total">
                 <span class="totalLabel">
                     Total <span class="textIva">(IVA incluido)</span>
                 </span>
-                <span class="totalPrice" id="finallPrice">1100 €</span>
+                <span class="totalPrice" id="finalPrice">1100 €</span>
             </div>
             <button class="btnComprar">Finalizar compra</button>
         </div>
