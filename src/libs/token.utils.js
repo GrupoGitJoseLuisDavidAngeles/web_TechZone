79b8ff36be660e1d90f3f52a120b08afd7e20a7d
JLRamirezBarrios <5268951@alu.murciaeduca.es>
2026-01-21 18:11:45 +0100

    API de pedidos_get y comprobación de inicio de sesión en index

--- src/libs/token.utils.js
+++ src/libs/token.utils.js
@@ -1,3 +1,5 @@
+import UsuarioService from '../services/usuario.service.js';
+
 export function decodeJWT(token) {
     if (!token) return null;
 
@@ -18,5 +20,29 @@ export function decodeJWT(token) {
         return JSON.parse(payloadJSON);
     } catch (e) {
         throw new Error('Error decodificando token:', e);
+    }
+}
+
+export async function checkTokenAndChangeLoginButton(token) {
+    const loginButton = document.getElementById('tLnkLogin');
+    const service = new UsuarioService();
+
+    if (!token) {
+        loginButton.textContent = 'Iniciar Sesión';
+        return;
+    }
+
+    try {
+        var userData = await service.getUserDataByToken(token);
+
+        if (userData && userData.nombre) {
+            loginButton.textContent = userData.nombre;
+            loginButton.href = '/profile/profile.php';
+        } else {
+            loginButton.textContent = 'Iniciar Sesión';
+        }
+    } catch (error) {
+        alert('Error al verificar el token: ' + error.message);
+        loginButton.textContent = 'Iniciar Sesión';
     }
 }
